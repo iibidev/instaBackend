902a2b178d28a4551d0c022bbbabd8627c73bd53
Ibai Amo <ibai.amo.ballesteros@gmail.com>
2025-06-02 08:52:49 +0200

    Update db.js

--- src/database/db.js
+++ src/database/db.js
@@ -1,6 +1,12 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+  throw new Error("❌ MONGO_URI no está definida en process.env");
+}
 
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
